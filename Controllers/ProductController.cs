4de10a044361c88f422f1d988b948e8c6ac1f84b
Matheus Gomes <matheus.gom23@yahoo.com>
2023-01-23 19:15:23 -0300

    Criação do ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestWebApi.Context;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
